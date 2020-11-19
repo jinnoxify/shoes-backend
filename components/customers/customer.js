a6e9780008a8038dcbe787dff78b178c778fb57d
Gerencia <gerencia@Mac-mini-de-Leaf.local>
2020-11-19 14:11:38 -0500

    changed port

--- components/customers/customer.js
+++ components/customers/customer.js
@@ -20,6 +20,16 @@ let quickStartCustomer = db => {
     country: "",
     isActive: Boolean
   });
+  db.collection("customers")
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, "=>", doc.data());
+      });
+    })
+    .catch(err => {
+      console.log("Error getting documents", err);
+    });
 };
 
 export default quickStartCustomer;
--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
 
-app.get("/new", (req, res) => {
-  res.send("Probando 1 2 3");
-});
+// app.get("/new", (req, res) => {
+//   res.send("Probando 1 2 3");
+// });
 
-exports.app = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app);
